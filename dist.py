cb1a626abe112c56b796fd1796786bcbe29ed9a3
ptrierweiler <ptrierweiler@gmail.com>
2025-09-04 16:33:50 -0500

    Create dist.py
    initial commit

---
+++ dist.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+input = sys.argv
+# input example 5,2 4,5 for dist
+# input example 5,2 4,5 m for mid
+x1,y1 = input[1].split(',')
+x2,y2 = input[2].split(',')
+
+print(input,len(input))
+print(f"x1: {x1}\ny1: {y1}\nx2: {x2}\ny2: {y2}")
+
+if len(input) == 3:
+    dist_func(x1,y1,x2,y2)
